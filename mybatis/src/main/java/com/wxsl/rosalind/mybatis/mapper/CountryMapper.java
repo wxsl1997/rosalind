1fc77f6704c70d5bf4ead692a9f2545776a4567a
wxsl5 <wxsl1997@163.com>
2022-11-20 14:10:55 +0800

    v-1.0.0 mybatis sql provider

--- mybatis/src/main/java/com/wxsl/rosalind/mybatis/mapper/CountryMapper.java
+++ mybatis/src/main/java/com/wxsl/rosalind/mybatis/mapper/CountryMapper.java
@@ -1,14 +1,17 @@
 package com.wxsl.rosalind.mybatis.mapper;
 
 import com.wxsl.rosalind.mybatis.model.Country;
+import lombok.experimental.UtilityClass;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.List;
 
 @Mapper
 public interface CountryMapper {
 
+    @SelectProvider(type = CountrySql.class, method = "findAll")
     List<Country> findAll();
 
     @Select("select * from country where id = #{id}")
@@ -19,4 +22,12 @@ public interface CountryMapper {
     void update(Country country);
 
     void deleteById(Long id);
+
+    @UtilityClass
+    class CountrySql {
+
+        public static String findAll() {
+            return "select * from country";
+        }
+    }
 }
