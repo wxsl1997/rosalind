8c29d7dfba8071300ba32ccfd7a829abe58f57fc
wxsl5 <wxsl1997@163.com>
2023-01-01 12:13:18 +0800

    v-1.0.0 redis dump

--- framework/src/test/java/com/wxsl/rosalind/framework/redis/RedisConfigurationTest.java
+++ framework/src/test/java/com/wxsl/rosalind/framework/redis/RedisConfigurationTest.java
@@ -2,13 +2,16 @@ package com.wxsl.rosalind.framework.redis;
 
 import com.wxsl.rosalind.base.BaseTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import redis.clients.jedis.Jedis;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+@Disabled
 class RedisConfigurationTest extends BaseTest {
 
     @Autowired
@@ -26,6 +29,23 @@ class RedisConfigurationTest extends BaseTest {
     @Test
     void jedis() {
         String ping = jedis.ping();
-        Assertions.assertNull(ping);
+        Assertions.assertNotNull(ping);
+    }
+
+    @Test
+    void dump() {
+        String key = "k1";
+        String value = "v1";
+
+        stringRedisTemplate.opsForValue().set(key, value);
+        // source database execute dump
+        byte[] source = stringRedisTemplate.dump(key);
+
+        // target database execute restore
+        //noinspection ConstantConditions
+        stringRedisTemplate.restore(key, source, 0, TimeUnit.SECONDS, true);
+
+        // 结果 检验
+        Assertions.assertEquals(value, stringRedisTemplate.opsForValue().get(key));
     }
 }
